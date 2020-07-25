eea764c5be59bf94018444321716ba52605c890a
zixiiu <faznky@gmail.com>
2020-07-24 21:39:55 -0400

    Updates

---
+++ precache-manifest.d6d7c3a4f530ed706a2c9993ce950e02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14632153afeefbcadadcadc9b6a9f660",
+    "url": "/Around-web/index.html"
+  },
+  {
+    "revision": "7a77d2d9751075275c6d",
+    "url": "/Around-web/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "c392ae356658b7b77e2a",
+    "url": "/Around-web/static/css/main.a88fe8c7.chunk.css"
+  },
+  {
+    "revision": "7a77d2d9751075275c6d",
+    "url": "/Around-web/static/js/2.803d2c7e.chunk.js"
+  },
+  {
+    "revision": "174263e5b9d3e85c08d4329eaeeb7640",
+    "url": "/Around-web/static/js/2.803d2c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c392ae356658b7b77e2a",
+    "url": "/Around-web/static/js/main.7433bc8f.chunk.js"
+  },
+  {
+    "revision": "b723e11cd1909fd0eedb",
+    "url": "/Around-web/static/js/runtime-main.ed07f724.js"
+  },
+  {
+    "revision": "bc8cc4eae3d3822562e825fb5cec5536",
+    "url": "/Around-web/static/media/blue-marker.bc8cc4ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Around-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
